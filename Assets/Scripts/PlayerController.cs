0cbc5a2c0fe08731ee0c848db87caeeb42531d2d
Pepikkvapil <kvapil.josef.matej@gmail.com>
2023-10-09 08:20:16 +0200

    Sprinting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    
+    public float playerStamina = 100.0f;
 
-    [SerializeField] float _Speed;
+
+    [SerializeField] private float maxStamina = 100.0f;
+    [SerializeField] private float dashCost = 20;
+
+    [HideInInspector] public bool hasRegenerated = true;
+    [HideInInspector] public bool weAreSprinting = false;
+
+    [SerializeField] private float staminaDrain = 0.5f;
+    [SerializeField] private float staminaRegen = 0.5f;
+
+    [SerializeField] private int RunSpeed = 6;
+    [SerializeField] private int normalRunSpeed = 4;
+
+    [SerializeField] private Image staminaProgressUI = null;
+    [SerializeField] private CanvasGroup sliderCanvasGroup = null;
+
 
     Vector2 _Movement;
 
@@ -22,8 +40,95 @@ public class PlayerController : MonoBehaviour
         _Movement = value.Get<Vector2>();
     }
 
-    private void FixedUpdate()
+    
+
+    void Update()
     {
-        _Rigidbody.velocity = _Movement * _Speed;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            weAreSprinting = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            weAreSprinting = false;
+        }
+        
+        if(weAreSprinting)
+        {
+            _Rigidbody.velocity = _Movement * RunSpeed;
+            playerStamina -= staminaDrain * Time.deltaTime;
+            UpdateStamina(1);
+
+            if (playerStamina <= 0)
+            {
+                hasRegenerated = false;
+
+                sliderCanvasGroup.alpha = 0;
+            }
+        }
+        else
+        {
+            _Rigidbody.velocity = _Movement * normalRunSpeed;
+        }
+
+        if (!weAreSprinting)
+        {
+            if (playerStamina <= maxStamina - 0.01)
+            {
+                playerStamina += staminaRegen * Time.deltaTime;
+                UpdateStamina(1);
+
+                if (playerStamina >= maxStamina)
+                {
+                    sliderCanvasGroup.alpha = 0;
+                    hasRegenerated = true;
+                }
+            }
+        }
+    }
+
+
+
+
+    //public void Sprinting()
+    //{
+    //    if (hasRegenerated)
+    //    {
+    //        weAreSprinting = true;
+    //        playerStamina -= staminaDrain * Time.deltaTime;
+    //        UpdateStamina(1);
+
+    //        if (playerStamina <= 0)
+    //        {
+    //            hasRegenerated = false;
+
+    //            sliderCanvasGroup.alpha = 0;
+    //        }
+    //    }
+    //}
+
+    public void StaminaDash()
+    {
+        if (playerStamina >= (maxStamina * dashCost / maxStamina))
+        {
+            playerStamina -= dashCost;
+
+            UpdateStamina(1);
+        }
+    }
+
+    void UpdateStamina(int value)
+    {
+        staminaProgressUI.fillAmount = playerStamina / maxStamina;
+
+        if (value == 0)
+        {
+            sliderCanvasGroup.alpha = 0;
+        }
+        else
+        {
+            sliderCanvasGroup.alpha = 1;
+        }
     }
 }
---
+++ Assets/StaminaController.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float playerStamina = 100.0f;
+
+    [SerializeField] private float maxStamina = 100.0f;
+    [SerializeField] private float dashCost = 20;
+    [HideInInspector] public bool hasRegenerated = true;
+    [HideInInspector] public bool weAreSprinting = false;
+
+    [SerializeField] private float staminaDrain = 0.5f;
+    [SerializeField] private float staminaRegen = 0.5f;
+
+    [SerializeField] private int slowedRunSpeed = 2;
+    [SerializeField] private int normalRunSpeed = 4;
+
+    [SerializeField] private Image staminaProgressUI = null; 
+    [SerializeField] private CanvasGroup sliderCanvasGroup = null;
+
+
+    private PlayerController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!weAreSprinting)
+        {
+            if(playerStamina <= maxStamina - 0.01)
+            {
+                playerStamina += staminaRegen * Time.deltaTime;
+                UpdateStamina(1);
+
+                if(playerStamina >= maxStamina)
+                {
+
+
+
+                    sliderCanvasGroup.alpha = 0;
+                    hasRegenerated = true;
+                }
+            }
+        }
+    }
+
+    public void Sprinting()
+    {
+        if(hasRegenerated)
+        {
+            weAreSprinting = true;
+            playerStamina -= staminaDrain * Time.deltaTime;
+            UpdateStamina(1);
+
+            if(playerStamina <= 0)
+            {
+                hasRegenerated = false;
+
+                sliderCanvasGroup.alpha = 0;
+            }
+        }
+    }
+
+
+    public void StaminaDash()
+    {
+        if(playerStamina >= (maxStamina * dashCost / maxStamina))
+        {
+            playerStamina -= dashCost;
+
+            UpdateStamina(1);
+        }
+    }
+
+
+    void UpdateStamina(int value)
+    {
+        staminaProgressUI.fillAmount = playerStamina / maxStamina;
+
+        if(value == 0)
+        {
+            sliderCanvasGroup.alpha = 0;
+        }
+        else
+        {
+            sliderCanvasGroup.alpha = 1;
+        }
+    }
+}
