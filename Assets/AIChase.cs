b797d5706114e1bfc70024e486e2d12e45137fc0
Pepikkvapil <kvapil.josef.matej@gmail.com>
2023-10-23 08:08:29 +0200

    Ai chase correction

--- Assets/AIChase.cs
+++ Assets/AIChase.cs
@@ -4,21 +4,20 @@ using UnityEngine;
 
 public class AIChase : MonoBehaviour
 {
-    public GameObject player;
+    private GameObject player;
     public float speed;
 
-    private float distance;
+   
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
